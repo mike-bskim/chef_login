fa1084a90db9f4f4d7f70fbae065c0fbc89c9aab
mike.bskim <mike.bskim@gmail.com>
2022-04-22 10:45:22 +0200

    login done

--- lib/login_app/login.dart
+++ lib/login_app/login.dart
@@ -31,34 +31,34 @@ class Login extends StatelessWidget {
         children: [
           MyButton(
             image: Image.asset('image/glogo.png'),
-            radius: 16,
             text: const Text(
               'Login with Google',
               style: TextStyle(color: Colors.black87, fontSize: 16.0),
             ),
             backgroundColor: Colors.white,
+            radius: 16,
             onPressed: () { debugPrint('Login with Google'); },
           ),
           _space,
           MyButton(
             image: Image.asset('image/flogo.png'),
-            radius: 16,
             text: const Text(
               'Login with Facebook',
               style: TextStyle(color: Colors.white, fontSize: 16.0),
             ),
             backgroundColor: const Color(0xFF334D92),
+            radius: 16,
             onPressed: () { debugPrint('Login with Facebook'); },
           ),
           _space,
           MyButton(
             image: const Icon(Icons.mail, color: Colors.white),
-            radius: 16,
             text: const Text(
               'Login with Email',
               style: TextStyle(color: Colors.white, fontSize: 16.0),
             ),
             backgroundColor: Colors.green,
+            radius: 16,
             onPressed: () { debugPrint('Login with Email'); },
           ),
         ],
